6f037cf79d8378433127f053e9139a1f95e0d4df
zlig <gdevillard@gmail.com>
2018-08-22 17:45:24 +0100

    Modifies code to use a correct API

--- sources/webapp/src/api/index.js
+++ sources/webapp/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export function fetchData() {
-  return axios.get('/api/services_status/').then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
+  return axios.get('/api/services/status/').then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
 }
 
 export function fetchSearchData(keyword) {
-  return axios.get('/api/services_status/' + keyword).then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
+  return axios.get('/api/services/status/' + keyword).then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
 }
