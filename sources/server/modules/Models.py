58dc26cc521569814c57b44dee3bf8f34675a0a2
zlig <gdevillard@gmail.com>
2018-09-03 10:09:45 +0100

    Separate models from the metrics from the service configuration

--- sources/server/modules/Models.py
+++ sources/server/modules/Models.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 
 
 class Server(Base):
+    ''' Server running the probe '''
     __tablename__ = 'server'
     id = Column(Integer, primary_key=True)
     hostname = Column(String(250), nullable=False)
@@ -26,10 +27,18 @@ class Version(Base):
 class Service(Base):
     __tablename__ = 'service'
     id = Column(Integer, primary_key=True)
-    date_time = Column(DateTime(timezone=True), default=datetime.datetime.utcnow)
-    timestamp = Column(BigInteger)
+    name = Column(String(128))
     uri = Column(String(1024))
-    latency = Column(Integer)
-    available = Column(Integer)
     server_id = Column(Integer, ForeignKey('server.id'))
     server = relationship(Server)
+
+
+class Metrics(Base):
+    __tablename__ = 'metrics'
+    id = Column(Integer, primary_key=True)
+    date_time = Column(DateTime(timezone=True), default=datetime.datetime.utcnow)
+    timestamp = Column(BigInteger)
+    latency = Column(Integer)
+    available = Column(Integer)
+    service_id = Column(Integer, ForeignKey('service.id'))
+    service = relationship(Service)
