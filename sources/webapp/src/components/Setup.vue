800defc4799805f874de584dbd1248237dbf6f3e
zlig <gdevillard@gmail.com>
2018-10-04 10:56:12 +0100

    Working on form

--- sources/webapp/src/components/Setup.vue
+++ sources/webapp/src/components/Setup.vue
@@ -81,13 +81,19 @@
                             <b-col sm="5"><label>Service Name</label></b-col>
                             <b-col sm="4"><label>URL</label></b-col>
                             <b-col sm="2"><label>Port</label></b-col>
-                            <b-col sm="1"><b-button @click="addRow"><strong> + </strong></b-button></b-col>
+                            <b-col sm="1"></b-col>
                           </b-row>
                           <b-row class="my-1" no-gutters v-if="services" v-for="(service, index) in services" v-bind:key="index">
                             <b-col sm="5">{{ service.name }}</b-col>
                             <b-col sm="4">{{ service.url }}</b-col>
                             <b-col sm="2">{{ service.port }}</b-col>
                             <b-col sm="1"></b-col>
+                          </b-row>
+                          <b-row class="my-1" no-gutters>
+                            <b-col sm="5"><b-form-input type="text"></b-form-input></b-col>
+                            <b-col sm="4"><b-form-input type="text"></b-form-input></b-col>
+                            <b-col sm="2"><b-form-input type="text"></b-form-input></b-col>
+                            <b-col sm="1"><b-button @click="addRow"><strong> + </strong></b-button></b-col>
                           </b-row>
                           <b-row class="my-1" no-gutters>
                             <b-col sm="10">
