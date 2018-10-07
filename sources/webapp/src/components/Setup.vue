9f8ba2cabda87200e3057742d58deac90dcf62a4
zlig <gdevillard@gmail.com>
2018-10-07 08:08:00 +0100

    Add delete row button

--- sources/webapp/src/components/Setup.vue
+++ sources/webapp/src/components/Setup.vue
@@ -87,7 +87,7 @@
                             <b-col sm="5">{{ service.name }}</b-col>
                             <b-col sm="4">{{ service.url }}</b-col>
                             <b-col sm="2">{{ service.port }}</b-col>
-                            <b-col sm="1"></b-col>
+                            <b-col sm="1"><b-button @click="deleteRow(index)"><strong> - </strong></b-button></b-col>
                           </b-row>
                           <b-row class="my-1" no-gutters>
                             <b-col sm="5"><b-form-input type="text" v-model="form.serviceName"></b-form-input></b-col>
