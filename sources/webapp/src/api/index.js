995d6b6d8f5c968e8e7f9803fe169b6853fa17bd
zlig <gdevillard@gmail.com>
2018-11-09 17:38:44 +0000

    Use new method

--- sources/webapp/src/api/index.js
+++ sources/webapp/src/api/index.js
@@ -8,10 +8,15 @@ export function fetchSearchData(keyword) {
   return axios.get('/api/services/status/' + keyword).then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
 }
 
+export function postPayload(url, data) {
+  var payload = { data: data }
+  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+  return axios.post(url, payload).then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
+}
+
 export function storeAdminPassword(password) {
   var payload = { password: password }
-  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-  return axios.post('/setup/password/', payload).then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
+  return postPayload('/setup/password/', payload)
 }
 
 export function storeGanalytics(uaid) {
@@ -24,10 +29,4 @@ export function storeServices(services) {
   var payload = { services: services }
   axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
   return axios.post('/setup/services/', payload).then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
-}
-
-export function postPayload(url, data) {
-  var payload = { data: data }
-  axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-  return axios.post(url, payload).then(response => { return response.data }).catch(error => { /* console.error(error); */ return Promise.reject(error) })
 }
