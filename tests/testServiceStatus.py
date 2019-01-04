1efc5fa718baf4ea0e373d3f872a93497abc8d3a
zlig <gdevillard@gmail.com>
2019-01-04 22:11:42 +0000

    Import module to test

--- tests/testServiceStatus.py
+++ tests/testServiceStatus.py
@@ -1,4 +1,10 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/../sources/server/')
 import unittest
+
+from modules.ServiceStatus import ServiceStatus
+
 
 class TestServiceStatus(unittest.TestCase):
     """ServiceStatus Unit Tests"""
