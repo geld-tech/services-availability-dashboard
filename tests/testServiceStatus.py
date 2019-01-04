56d5eee7c22435e9fc914975d54b129cb0b78fc9
zlig <gdevillard@gmail.com>
2019-01-04 22:19:24 +0000

    Increase coverage

--- tests/testServiceStatus.py
+++ tests/testServiceStatus.py
@@ -9,9 +9,25 @@ from modules.ServiceStatus import ServiceStatus
 class TestServiceStatus(unittest.TestCase):
     """ServiceStatus Unit Tests"""
 
-    def test_true(self):
-        """Stub test"""
-        self.assertTrue(True)
+    def test_init(self):
+        """Instatiation"""
+        service_status = ServiceStatus()
+
+    def test_get(self):
+        """Data getter"""
+        service_status = ServiceStatus()
+        self.assertEqual({}, service_status.get())
+
+    def test_poll_metrics(self):
+        """Poll Metrics"""
+        service_status = ServiceStatus()
+        self.assertEqual({}, service_status.poll_metrics({}))
+
+    def test_collect_metrics(self):
+        """Collect Metrics"""
+        service_status = ServiceStatus()
+        service_status.collect_metrics({})
+        self.assertEqual({}, service_status.get())
 
 
 if __name__ == '__main__':
