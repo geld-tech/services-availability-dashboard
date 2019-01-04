0b99fdcf668ae9fa8b881611478210645f0986a6
zlig <gdevillard@gmail.com>
2019-01-04 22:01:38 +0000

    Adds unit tests stub

---
+++ tests/ServiceStatus_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestServiceStatus(unittest.TestCase):
+    """ServiceStatus Unit Tests"""
+
+    def test_true(self):
+        """Stub test"""
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
