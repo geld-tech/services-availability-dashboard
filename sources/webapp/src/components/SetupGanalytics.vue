3b8c2f375e073314d0c9fdcef1465db008a3f1da
zlig <gdevillard@gmail.com>
2019-02-25 15:56:40 +0000

    Ganalytics in their own component

---
+++ sources/webapp/src/components/SetupGanalytics.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+        <h2>Google Analytics</h2>
+        <div v-if="ganalyticsIdSet" class="pt-1">
+            <p>Analytics UA ID set successfully!</p>
+        </div>
+        <div v-else>
+            <p>Enter the Google Analytics UA ID in the field below, then press Submit</p>
+            <b-form @submit="onSubmitGaId" @reset="onResetGaId" id="uaid" v-if="show">
+                <b-container fluid>
+                  <b-row class="my-1">
+                    <b-col sm="5">
+                        <label>Google Analytics UA ID</label>
+                    </b-col>
+                    <b-col sm="7">
+                        <b-form-input type="text" v-model="form.uaid" id="uaIdInput" required></b-form-input>
+                    </b-col>
+                  </b-row>
+                  <b-row class="my-1">
+                    <b-col sm="12">
+                      <b-button type="reset" variant="danger" v-bind:disabled="disableGaIdButtons" id="uaidClearButton">Clear</b-button>
+                      <b-button type="submit" variant="primary" v-bind:disabled="disableGaIdButtons" id="uaidAdminButton">Submit</b-button>
+                    </b-col>
+                  </b-row>
+                </b-container>
+            </b-form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { storeGanalytics } from '@/api'
+
+export default {
+  name: 'SetupGanalytics',
+  data () {
+    return {
+      form: {
+        uaid: ''
+      },
+      error: '',
+      show: true
+    }
+  },
+  computed: {
+    disableGaIdButtons() {
+      return (this.form.uaid === '')
+    }
+  },
+  methods: {
+    onSubmitGaId(evt) {
+      evt.preventDefault()
+      var uaid = this.sanitizeString(this.form.uaid)
+      this.form.uaid = ''
+      this.error = ''
+      if (uaid !== '') {
+        /* Trick to reset/clear native browser form validation state */
+        this.show = false
+        this.$nextTick(() => { this.show = true })
+        /* Fetching the data */
+        storeGanalytics(uaid)
+          .then(response => {
+            this.$emit('set-ganalytics-uaid', true)
+          })
+          .catch(err => {
+            this.error = err.message
+          })
+      } else {
+        this.error = 'GA UA ID cant be empty!'
+      }
+    },
+    onResetGaId(evt) {
+      evt.preventDefault()
+      /* Reset our form values */
+      this.form.uaid = ''
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false
+      this.$nextTick(() => { this.show = true })
+    },
+    sanitizeString(input) {
+      input = input.trim()
+      input = input.replace(/[`~!$%^&*|+?;:'",\\]/gi, '')
+      input = input.replace('/', '')
+      input = input.trim()
+      return input
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  font-weight: normal;
+}
+.container {
+  max-width: 1200px;
+  margin:  0 auto;
+}
+.alerting {
+  margin: 0 auto;
+  text-align: center;
+  display: block;
+  line-height: 15px;
+}
+</style>
