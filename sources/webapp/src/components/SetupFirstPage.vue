71601e986744cac58dedcff8dff8f6cc689565ff
zlig <gdevillard@gmail.com>
2019-02-25 15:38:53 +0000

    Components for first page

---
+++ sources/webapp/src/components/SetupFirstPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>First Setup</h2>
+        <p>Welcome to the first setup!</p>
+        <p>The following pages will guide you through the configuration to start using you service.</p>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+h2 {
+  font-weight: normal;
+}
+</style>
