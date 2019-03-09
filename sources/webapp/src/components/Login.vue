8a58634e17ae08567ab466eca04d969f49b1da31
zlig <gdevillard@gmail.com>
2019-03-09 08:08:00 +0100

    DOM warning

--- sources/webapp/src/components/Login.vue
+++ sources/webapp/src/components/Login.vue
@@ -23,7 +23,7 @@
                         <label>Password</label>
                     </b-col>
                     <b-col sm="7">
-                        <b-form-input type="password" autocomplete="new-password" v-model="form.adminPassword" id="adminPassword" required></b-form-input>
+                        <b-form-input type="password" autocomplete="current-password" v-model="form.adminPassword" id="adminPassword" name="adminPassword" required></b-form-input>
                     </b-col>
                   </b-row>
                   <b-row class="my-1">
