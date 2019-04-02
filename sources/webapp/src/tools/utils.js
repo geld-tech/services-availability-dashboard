5e5d89dae7263e479bb1813f394d982beced0a3f
zlig <gdevillard@gmail.com>
2019-04-02 21:33:43 +0100

    Replace / by _wq

--- sources/webapp/src/tools/utils.js
+++ sources/webapp/src/tools/utils.js
@@ -2,7 +2,7 @@ export function sanitizeString(input) {
   input = input.trim()
   input = input.replace(/[`~!$%^&*|+?;:'",\\]/gi, '')
   input = input.replace(/\/\//g, '_')
-  input = input.replace('/', '')
+  input = input.replace('/', '_')
   input = input.trim()
   return input
 }
