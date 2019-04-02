ba96cc321d3ad6b71462fcc45a90e3d933dc7eef
zlig <gdevillard@gmail.com>
2019-04-02 21:26:42 +0100

    Don't store //

--- sources/webapp/src/tools/utils.js
+++ sources/webapp/src/tools/utils.js
@@ -1,6 +1,7 @@
 export function sanitizeString(input) {
   input = input.trim()
   input = input.replace(/[`~!$%^&*|+?;:'",\\]/gi, '')
+  input = input.replace(/\/\//g, '_')
   input = input.replace('/', '')
   input = input.trim()
   return input
